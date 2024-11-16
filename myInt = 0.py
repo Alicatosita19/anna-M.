c7e38840499926adba2ba4632519cc1dfcfbdbca
Alicatosita19 <alicatosita19@icloud.com>
2024-11-16 10:39:23 -0800

    Add files via upload

---
+++ myInt = 0.py
@@ -0,0 +1,14 @@
+myList = ["Amazing", "Non stop", "Nifty", "Awesome",]
+
+for item in myList:
+    print(item)
+print("\n")
+myList = ["Kind", "Wonderful", "Optamistic", "Krazy",]
+
+for item in myList:
+    print(item)
+print("\n")
+myList = ["Magnificent", "Athletic", "Intelligent", "Never gave up", "Analitical", "Radical", "Dramatic", "Imaginative"]
+
+for item in myList:
+    print(item)
